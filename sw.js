eeaeecb6b87698c892dd3dc64051ffa5131ac4d0
tianzhongxinnaizhu-cyber <tianzhongxinnaizhu@gmail.com>
2026-09-18 12:13:38 +0900

    Update Baseball GM to v3.32

--- sw.js
+++ sw.js
@@ -1,43 +1,5 @@
-const CACHE = "baseball-gm-v3.31-pages-2";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./manifest.webmanifest",
-  "./icon.svg",
-  "./data/part-00.txt",
-  "./data/part-01.txt",
-  "./data/part-02.txt",
-  "./data/part-03.txt",
-  "./data/part-04.txt",
-  "./data/part-05.txt",
-  "./data/part-06.txt",
-  "./data/part-07.txt",
-  "./data/part-08.txt"
-];
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE)
-      .then(cache => cache.addAll(ASSETS))
-      .then(() => self.skipWaiting())
-  );
-});
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
-      .then(() => self.clients.claim())
-  );
-});
-self.addEventListener("fetch", event => {
-  if (event.request.method !== "GET") return;
-  event.respondWith(
-    caches.match(event.request).then(hit => hit || fetch(event.request).then(response => {
-      const copy = response.clone();
-      caches.open(CACHE).then(cache => cache.put(event.request, copy));
-      return response;
-    }).catch(() => {
-      if (event.request.mode === "navigate") return caches.match("./index.html");
-      return Response.error();
-    }))
-  );
-});
+const CACHE="baseball-gm-v3.32-pages-1";
+const ASSETS=["./", "./index.html", "./manifest.webmanifest", "./icon.svg", "./data/part-00.txt", "./data/part-01.txt", "./data/part-02.txt", "./data/part-03.txt", "./data/part-04.txt", "./data/part-05.txt", "./data/part-06.txt", "./data/part-07.txt", "./data/part-08.txt"];
+self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(hit=>hit||(e.request.mode==="navigate"?caches.match("./index.html"):Response.error()))))});
