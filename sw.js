83ae72a90394f33fefa5f29f5577331acf79b63e
tianzhongxinnaizhu-cyber <tianzhongxinnaizhu@gmail.com>
2026-09-18 12:27:17 +0900

    Update offline cache for v3.33

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="baseball-gm-v3.32-direct-1";
+const CACHE="baseball-gm-v3.33-direct-1";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
